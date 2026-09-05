6485aba4a2911b7556c53294ec6077635708b735
rajashreebtech888-alt <rajashreebtech888@gmail.com>
2026-09-05 20:15:48 +0530

    Successfully configured DVC pipeline with MLflow

---
+++ src/train.py
@@ -0,0 +1,26 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+mlflow.set_experiment("MLOps_Demo_Experiment")
+
+with mlflow.start_run():
+    iris = load_iris()
+    X_train, X_test, y_train, y_test = train_test_split(
+        iris.data, iris.target, test_size=0.2, random_state=42
+    )
+
+    n_estimators = 50
+    mlflow.log_param("n_estimators", n_estimators)
+
+    clf = RandomForestClassifier(n_estimators=n_estimators, random_state=42)
+    clf.fit(X_train, y_train)
+
+    predictions = clf.predict(X_test)
+    acc = accuracy_score(y_test, predictions)
+    mlflow.log_metric("accuracy", acc)
+
+    print(f"Model trained with Accuracy: {acc}")
